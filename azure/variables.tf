39b3eaecd5e23f0c414308f14be374ca4062cd9c
mjombanderitu <80721502+mjombanderitu@users.noreply.github.com>
2026-01-06 13:45:43 +0300

    Add Azure variables.tf

---
+++ azure/variables.tf
@@ -0,0 +1,17 @@
+# Variables for Azure Terraform deployment
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region to deploy resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
